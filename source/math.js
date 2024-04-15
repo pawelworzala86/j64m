da8347797f42978e349587fe67ca74d51f72e2f7
pawelworzala86 <pawelworzala86@gmail.com>
2024-04-15 16:35:16 +0200

    auto save

--- source/math.js
+++ source/math.js
@@ -6,65 +6,38 @@ function Macro_Math_pomnoz(aram11,aram22,aret33){
     fstp aret33
 }
 
+function Macro_Math_dodaj(aram11,aram22,aret33){
+    fld aram11
+    fadd aram22
+    fstp aret33
+}
+
+function Macro_Math_podziel(aram11,aram22,aret33){
+    fld aram11
+    fdiv aram22
+    fstp aret33
+}
+
+function Macro_Math_odejmnij(aram11,aram22,aret33){
+    fld aram11
+    fsub aram22
+    fstp aret33
+}
+
+
+
 /*
-
-.code
-    Macro_Math_dodaj macro aram11,aram22,aret33
-    
-
-    fld qword ptr aram11
-
-    fadd qword ptr aram22
-
-    fstp qword ptr aret33
-
-
-    endm
-
-
-
-.code
-    Macro_Math_podziel macro aram11,aram22,aret33
-    
-
-    fld qword ptr aram11
-
-    fdiv qword ptr aram22
-
-    fstp qword ptr aret33
-
-
-    endm
-
-
-
-.code
-    Macro_Math_odejmnij macro aram11,aram22,aret33
-    
-
-    fld qword ptr aram11
-
-    fsub qword ptr aram22
-
-    fstp qword ptr aret33
-
-
-    endm
-
-
-
-
     
 
 .code
     Macro_Math_sin macro aram11,aret33
     
 
-    fld qword ptr aram11
+    fld aram11
 
     fsin
 
-    fstp qword ptr aret33
+    fstp aret33
 
 
     endm
@@ -75,11 +48,11 @@ function Macro_Math_pomnoz(aram11,aram22,aret33){
     Macro_Math_cos macro aram11,aret33
     
 
-    fld qword ptr aram11
+    fld aram11
 
     fcos
 
-    fstp qword ptr aret33
+    fstp aret33
 
 
     endm
@@ -90,11 +63,11 @@ function Macro_Math_pomnoz(aram11,aram22,aret33){
     Macro_Math_sqrt macro aram11,aret33
     
 
-    fld qword ptr aram11
+    fld aram11
 
     fsqrt
 
-    fstp qword ptr aret33
+    fstp aret33
 
 
     endm
@@ -111,11 +84,11 @@ function Macro_Math_pomnoz(aram11,aram22,aret33){
 
     Macro_Math_cos aram11,stp2
 
-    fld qword ptr stp1
+    fld stp1
 
-    fdiv qword ptr stp2
+    fdiv stp2
 
-    fstp qword ptr aret33
+    fstp aret33
 
 
     endm
@@ -126,11 +99,11 @@ function Macro_Math_pomnoz(aram11,aram22,aret33){
     Macro_Math_Deg2Rad macro aram11,aret33
     
 
-    fld qword ptr aram11
+    fld aram11
 
-    fmul qword ptr deg2rad
+    fmul deg2rad
 
-    fstp qword ptr aret33
+    fstp aret33
 
 
     endm
